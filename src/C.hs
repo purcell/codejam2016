f0ddd55e672f068e2bf47f0312c2fb96a62b31bb
Steve Purcell <steve@sanityinc.com>
2016-04-09 21:21:22 +1200

    Working solution for part 1 of C

--- src/C.hs
+++ src/C.hs
@@ -1,49 +1,52 @@
 module C where
 
-import           Control.Monad (replicateM)
-import           Data.List     (find)
-import           Data.Maybe    (catMaybes, fromJust)
+import           Control.Monad       (replicateM)
+--import           Data.Char           (intToDigit)
+import           Data.List           (find)
+import           Data.Maybe          (catMaybes, fromJust)
+import           Data.Numbers.Primes (isPrime)
+--import           Numeric             (showIntAtBase)
 
-toIntInBase :: Int -> String -> Int
+toIntInBase :: Integer -> String -> Integer
 toIntInBase base ds = sum $ zipWith f [0..] (reverse ds)
   where
-    f :: Int -> Char -> Int
+    f :: Integer -> Char -> Integer
     f n '1' = base ^ n
     f _ '0' = 0
     f _ c = error $ "bad char: " ++ show c
 
 
--- naïve primality test
-prime :: (Integral a) => a -> Bool
-prime k | k <=1 = False
-        | otherwise = 0 `notElem` (map (mod k)[2..k-1])
-
-data Jamcoin = Jamcoin String [Int]
+data Jamcoin = Jamcoin String [Integer]
                deriving Show
 
 toJamcoin :: String -> Maybe Jamcoin
 toJamcoin digits@('1':ds) | last ds == '1'
-  = if all (not . prime) interps
-    then Just (Jamcoin digits (fromJust . nonTrivialDivisor <$> interps))
+  = if all (not . isPrime) interps
+    then Just (Jamcoin digits (nonTrivialDivisor <$> interps))
     else Nothing
   where
     interps = interpretations digits
 toJamcoin _ = Nothing
 
-interpretations :: String -> [Int]
+interpretations :: String -> [Integer]
 interpretations digits = map (`toIntInBase` digits) [2..10]
 
-nonTrivialDivisor :: Int -> Maybe Int
-nonTrivialDivisor n = find (\i -> (n `mod` i) == 0) [2..n-2]
+nonTrivialDivisor :: Integer -> Integer
+nonTrivialDivisor n = fromJust $ find (\i -> (n `mod` i) == 0) [2..n-2]
 
-
-possibleJamcoins :: Int -> [String]
+possibleJamcoins :: Integer -> [String]
 possibleJamcoins n = do
-  middle <- replicateM (n - 2) "01"
+  middle <- replicateM (fromIntegral (n - 2)) "01"
   return $ "1" ++ middle ++ "1"
+--possibleJamcoins n = map toBinString [min, (min+2)..max]
+--  where
+--    min = 1 + 2 ^(n-1)
+--    max = 2^n - 1
+--    toBinString ds = (showIntAtBase 2 intToDigit ds) ""
+--
 
-findJamcoins :: Int -> Int -> [Jamcoin]
-findJamcoins j n = take j $ catMaybes $ toJamcoin <$> possibleJamcoins n
+findJamcoins :: Integer -> Integer -> [Jamcoin]
+findJamcoins j n = take (fromIntegral j) $ catMaybes $ toJamcoin <$> possibleJamcoins n
 
 
 solve :: String -> [Jamcoin]
