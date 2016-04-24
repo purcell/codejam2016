7fe9681ff4daace0327bd43876e2cc7288e8c6e1
Steve Purcell <steve@sanityinc.com>
2016-04-24 13:59:17 +1200

    Solved Round 1A problem B (large)
    Cheated using this insight: http://codegolf.stackexchange.com/a/78303

--- src/Round1A/B.hs
+++ src/Round1A/B.hs
@@ -1,68 +1,17 @@
 module Round1A.B where
 
-import           Data.List  (delete, sort)
-import           Data.Map   (Map)
-import qualified Data.Map   as M
-import           Data.Maybe (fromJust)
+import           Data.List (group, sort)
 
 type Height = Int
 
 data Case = Case Int [[Height]]
 data Solution = Solution [Height]
 
-data Strip = Row Int | Col Int deriving (Eq, Show)
-
-data Grid = Grid { gridSize        :: Int
-                 , gridEmptyStrips :: [Strip]
-                 , gridContents    :: Map (Int, Int) Height }
-            deriving Show
-
-gridValues :: Grid -> Strip -> [Maybe Height]
-gridValues g s = map (flip M.lookup (gridContents g)) $ stripCoords g s
-
-stripCoords :: Grid -> Strip -> [(Int, Int)]
-stripCoords g s = case s of
-                    Row x -> zip (repeat x) [1..n]
-                    Col y -> zip [1..n] (repeat y)
-  where n = gridSize g
-
-emptyGrid :: Int -> Grid
-emptyGrid n = Grid n [c x | c <- [Row, Col], x <- [1..n]] M.empty
-
-place :: Grid -> Strip -> [Height] -> Maybe Grid
-place g s hs = if all emptyOrMatch updates then
-                 Just (g { gridEmptyStrips = delete s $ gridEmptyStrips g
-                         , gridContents = gridContents g `M.union` M.fromList updates })
-               else Nothing
-  where
-    emptyOrMatch (pos, h) = case M.lookup pos (gridContents g) of
-                              Just h' | h' == h -> True
-                              Nothing           -> True
-                              _                 -> False
-    updates = zip (stripCoords g s) hs
-
-placeAll :: Grid -> [[Height]] -> [Grid]
-placeAll g [] = return g
-placeAll g (l:ls) = do
-  strip <- nextEmptyStrips g
-  case place g strip l of
-    Just g' -> placeAll g' ls
-    _ -> []
-
-nextEmptyStrips :: Grid -> [Strip]
-nextEmptyStrips g = take 2 (filter isRow strips) ++ take 2 (filter (not . isRow) strips)
-  where
-    isRow (Row _) = True
-    isRow (Col _) = False
-    strips = gridEmptyStrips g
-
-arrangements :: Case -> [Grid]
-arrangements (Case n ls) = placeAll (emptyGrid n) $ sort ls
+frequencies :: (Ord a, Eq a) => [a] -> [(a, Int)]
+frequencies = map (\g -> (head g, length g)) . group . sort
 
 solve :: Case -> Solution
-solve c = Solution $ map fromJust $ gridValues grid strip
-  where grid = head $ arrangements c
-        strip = head $ gridEmptyStrips grid
+solve (Case _ lists) = Solution $ map fst $ filter (odd . snd) $ frequencies (concat lists)
 
 instance Show Solution where
   show (Solution xs) = unwords $ map show xs
